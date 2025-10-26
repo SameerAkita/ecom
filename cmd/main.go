99e602dc15115f58b50645d083e60fb848552844
Sameer <akita.sameer1@gmail.com>
2025-10-26 17:43:48 +0900

    db file

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,21 @@ package main
 import (
 	"log"
 	"github.com/SameerAkita/ecom/cmd/api"
+	"github.com/SameerAkita/ecom/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User: "root",
+		Passwd: "asd",
+		Addr: "127.0.1:3306",
+		DBName: "ecom",
+		Net: "tcp",
+		AllowNativePasswords: true,
+		ParseTime: true,
+	})
+
 	server := api.NewAPIServer(":8080", nil)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
---
+++ db/db.go
@@ -0,0 +1,16 @@
+package db
+
+import (
+	"log"
+	"database/sql"
+	"github.com/go-sql-driver/mysql"
+)
+
+func NewMySQLStorage(cfg mysql.Config) (*sql.DB, error) {
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db, nil
+}
