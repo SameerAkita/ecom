cc92b5c28f3f29b5d7ba012d4138f1e5d0b385de
Sameer <akita.sameer1@gmail.com>
2025-10-26 18:18:55 +0900

    config.env

--- cmd/main.go
+++ cmd/main.go
@@ -5,20 +5,24 @@ import (
 	"github.com/SameerAkita/ecom/cmd/api"
 	"github.com/SameerAkita/ecom/db"
 	"github.com/go-sql-driver/mysql"
+	"github.com/SameerAkita/ecom/config"
 )
 
 func main() {
 	db, err := db.NewMySQLStorage(mysql.Config{
-		User: "root",
-		Passwd: "asd",
-		Addr: "127.0.1:3306",
-		DBName: "ecom",
-		Net: "tcp",
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
 		AllowNativePasswords: true,
-		ParseTime: true,
+		ParseTime:            true,
 	})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	server := api.NewAPIServer(":8080", nil)
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
--- config/env.go
+++ config/env.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"os"
+	"fmt"
+)
+
 type Config struct {
 	PublicHost string
 	Port       string
@@ -9,6 +14,23 @@ type Config struct {
 	DBName     string
 }
 
+var Envs = initConfig()
+
 func initConfig() Config {
-	
+	return Config {
+		PublicHost: getEnv("PUBLIC_HOST", "http://localhost"),
+		Port: getEnv("PORT", "8080"),
+		DBUser: getEnv("DB_USER", "root"),
+		DBPassword: getEnv("DB_PASSWORD", "mypassword"),
+		DBAddress: fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
+		DBName: getEnv("DB_NAME", "ecom"),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return fallback
 }
