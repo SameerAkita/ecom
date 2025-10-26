37fee333131d418b190d8420b730f2f2110a40c9
Sameer <akita.sameer1@gmail.com>
2025-10-26 16:21:20 +0900

    api server

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"github.com/SameerAkita/ecom/cmd/api"
+)
+
+func main() {
+	server := api.APIServer(":8080", nil)
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
